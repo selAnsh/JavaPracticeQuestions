7b7fbb2791dfa351ab5db1a97b24dcf9accd5aed
selAnsh <anshulmittal375@gmail.com>
2024-01-02 17:34:09 +0530

    New mathematical numbers pushed

--- MagicalNumbers.java
+++ MagicalNumbers.java
@@ -1,13 +1,72 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class MagicalNumbers {
 
     static utilities ut = new utilities();
-    static int num=0;
+/**
+ * Tech number 
+ * A even number when divided into two parts and added, then square of added number is same
+ * Eg. 2025 -> 20,25
+ * (20 + 25)^2
+ * 45^2 = 2025
+ */
+    public static boolean tech(long n){
+        long arr[] = utilities.numToArray(n);
+        if(arr.length % 2 !=0) {
+            return false;
+        }
+        long x = utilities.arrayToNum(Arrays.copyOfRange(arr, 0, arr.length/2));
+        long y = utilities.arrayToNum(Arrays.copyOfRange(arr,arr.length/2,arr.length));
 
-    // 1. To reverse the number
+        
+        return n==Math.pow(x+y, 2)?true:false;
+    }
+    /*A number is sunny when n+1 is a perfect square
+     * 80
+     * 80 + 1 = 81
+     * Square root of 81 = 9 
+     */
+    public static boolean sunny(long n){
+        System.out.println(Math.sqrt(n+1));
+        System.out.println(Math.floor(Math.sqrt(n+1)));     
+        return Math.floor(Math.sqrt(n+1))-Math.sqrt(n+1)==0? true: false;
+    }
+/**
+ * 3. Peterson Number -> sum of digits of factorial is equal to the number itself
+ * Eg. 145 
+ * 1! + 4! + 5! => 1 + 24 + 120 => 145
+ * 145 =145
+ *  So it is peterson number
+ * */
+    public static boolean peterson(long n) {
+        long add=0;
+        long numArr[] = utilities.numToArray(n);
+        for (int i =0;i< numArr.length;i++) {
+            add+=utilities.fact(numArr[i]);
+        }
+        return add==n?true:false;
+    }
+
+    /**2. Automorphic number square of the number contains the number in the starting digits
+    Eg. Square of 25 is 625
+    Which contains 25 at last
+    **/
+    public static boolean automorphic(long n){
+        long sqr = (long) Math.pow(n, 2);
+        long numArr[] = ut.numToArray(n);
+        long sqrArr[] = ut.numToArray(sqr);
+        for (int i =0;i< numArr.length ;i ++){
+            if(numArr[i]!=sqrArr[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // 1. To reverse the number Input - 4550 Output - 554
     public static long reverse(long n){
-
+        
         long len[] = ut.numToArray(n);
         
         int reversedNum=0;
@@ -30,5 +89,10 @@ public class MagicalNumbers {
             System.out.println("This is the program to find various MATHEMATICAL numbers");
             long n =s.nextLong();
             System.out.println(reverse(n));
+            System.out.println(automorphic(n)?"Automorphic":"not automorphic");
+            System.out.println(peterson(n)?"Peterson":"not Peterson");
+            System.out.println(tech(n)?"Tech":"not tech");
+            System.out.println(sunny(n)?"Sunny" : "Not sunny");
+            
     }
 }
--- utilities.java
+++ utilities.java
@@ -1,25 +1,41 @@
+import java.util.Arrays;
+
 public class utilities {
-    static int num=0;
-       
+    static boolean flag = false;
+    static int len =0;
     //Common utility to calculate length
+    //Flag to decide if set value of len to 0 and restart
     public static int lengthNum(long a) {
+        if(!flag){
+            len =0;
+            flag = true;
+        }
         if (a>0) {
-            num++;
+            len++;
             lengthNum(a/10);
         }
-        return num;
+        flag = false;
+        return len;
     }
 
+    public static long fact(long a) {
+        if (a>0){
+            return a*fact(a-1);
+        }
+        return 1;
+    }
     
     //Common utility to convert number to array
     public static long[] numToArray(long n){
-            int count =0;
-            long arr[] =  new long[lengthNum(n)];
+        int count =0;
+        long arr[] =  new long[lengthNum(n)];
             while(n>0) {
                 arr[count] = n%10;
                 n/=10;
                 count++;
             }
+            
+        System.out.println(Arrays.toString(arr));
             return arr;
     }
     public static long arrayToNum(long[] arr){
@@ -31,7 +47,6 @@ public class utilities {
             count++;
         
         }
-        
         return val;        
 
      }
